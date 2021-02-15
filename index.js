3058fcfa5b3c18d61bcdab16d56fa3ba4cfe7002
amrit@fusmeachines.com <ammuffying@gmail.com>
2021-02-15 13:11:20 +0545

    - Yearly Tax

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ btn.addEventListener("click", function () {
   let monthlyIncome = document.getElementById("monthly_income").value;
   let yearlyIncome = (document.getElementById("yearly_income").value =
     monthlyIncome * 12);
+  let yearlyTax = (document.getElementById("yearly_tax").value = getYearlyTax(
+    yearlyIncome
+  ));
 });
 
 let reset = document.getElementById("reset");
@@ -12,3 +15,16 @@ reset.addEventListener("click", function () {
   let yearly_income = (document.getElementById("yearly_income").value = "");
 });
 
+function getYearlyTax(yearlyIncome) {
+  if (yearlyIncome <= 400000) {
+    return (yearlyIncome * 1) / 100;
+  } else if (yearlyIncome > 400000 && 500000) {
+    return ((yearlyIncome - 400000) * 10) / 100;
+  } else if (yearlyIncome > 500000 && yearlyIncome <= 700000) {
+    return ((yearlyIncome - 400000) * 20) / 100;
+  } else if (yearlyIncome > 700000 && yearlyIncome <= 2000000) {
+    return ((yearlyIncome - 400000) * 30) / 100;
+  } else if (yearlyIncome > 2000000) {
+    return ((yearlyIncome - 400000) * 36) / 100;
+  }
+}
