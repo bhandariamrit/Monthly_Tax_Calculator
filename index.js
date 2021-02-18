df699063fb3d804ad46fee7899772365f2f8ba45
amrit@fusmeachines.com <ammuffying@gmail.com>
2021-02-18 20:41:47 +0545

    - Added Radio button for Married and Unmarried. -  Calculation for  Tax slab.

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 let btn = document.getElementById("btn");
 btn.addEventListener("click", function () {
+  let genderStaus = document.querySelector("input[name='gender']:checked")
+    .value;
+
   let monthlyIncome = document.getElementById("monthly_income").value; //monthly income
 
   let yearlyIncome = (document.getElementById("yearly_income").value = //yearly income
     monthlyIncome * 12);
-  let yearlyTax = (document.getElementById("yearly_tax").value = getYearlyTax(
-    yearlyIncome
-  ));
+  let yearlyTax = 0;
+  if (genderStaus === "Unmarried") {
+    yearlyTax = getYearlyUnmarriedTax(yearlyIncome);
+  } else {
+    yearlyTax = getMarriedTax(yearlyIncome);
+  }
+  let totalYearlyTax = (document.getElementById(
+    "yearly_tax"
+  ).value = yearlyTax);
+
+  console.log(yearlyTax);
 
   let monthlyTax = (document.getElementById("monthly_tax").value =
-    getYearlyTax(yearlyIncome) / 12); //monthly tax
+    totalYearlyTax / 12); //monthly tax
 
   let cashInHand = (document.getElementById("monthly_salary").value =
-    monthlyIncome - monthlyTax);
+    monthlyIncome - monthlyTax); // cash in hand
 });
-
+//// Reset Function
 let reset = document.getElementById("reset");
 reset.addEventListener("click", function () {
   let monthlyIncome = (document.getElementById("monthly_income").value = "");
@@ -23,16 +34,32 @@ reset.addEventListener("click", function () {
   let monthlyTax = (document.getElementById("monthly_tax").value = "");
 });
 
-function getYearlyTax(yearlyIncome) {
+//// Unmarried
+function getYearlyUnmarriedTax(yearlyIncome) {
   if (yearlyIncome <= 400000) {
-    return (yearlyIncome * 1) / 100;
-  } else if (yearlyIncome > 400000 && 500000) {
-    return ((yearlyIncome - 400000) * 10) / 100;
+    return yearlyIncome * 0.01;
+  } else if (yearlyIncome > 400000 && yearlyIncome <= 500000) {
+    return 4000 + (yearlyIncome - 400000) * 0.1;
   } else if (yearlyIncome > 500000 && yearlyIncome <= 700000) {
-    return ((yearlyIncome - 400000) * 20) / 100;
+    return 4000 + 10000 + (yearlyIncome - 500000) * 0.2;
   } else if (yearlyIncome > 700000 && yearlyIncome <= 2000000) {
-    return ((yearlyIncome - 400000) * 30) / 100;
+    return 4000 + 10000 + 40000 + (yearlyIncome - 700000) * 0.3;
   } else if (yearlyIncome > 2000000) {
-    return ((yearlyIncome - 400000) * 36) / 100;
+    return 4000 + 10000 + 40000 + 390000 + (yearlyIncome - 2000000) * 0.36;
+  }
+}
+/// Married
+function getMarriedTax(yearlyIncome) {
+  if (yearlyIncome <= 450000) {
+    return yearlyIncome * 0.01;
+  } else if (yearlyIncome > 450000 && yearlyIncome <= 550000) {
+    let tax = 4500 + (yearlyIncome - 450000) * 0.1;
+    return 4500 + (yearlyIncome - 450000) * 0.1;
+  } else if (yearlyIncome > 550000 && yearlyIncome <= 750000) {
+    return 4500 + 10000 + (yearlyIncome - 550000) * 0.2;
+  } else if (yearlyIncome > 750000 && yearlyIncome <= 2000000) {
+    return 4500 + 10000 + 40000 + (yearlyIncome - 750000) * 0.3;
+  } else if (yearlyIncome > 2000000) {
+    return 4500 + 10000 + 40000 + 375000 + (yearlyIncome - 2000000) * 0.36;
   }
 }
